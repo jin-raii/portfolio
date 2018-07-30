5b8a52359f83354cb0c5eb1dc6d760b8bced2587
jin <jinrai5777@gmail.com>
2018-07-30 14:22:30 +0545

    adding files to github

---
+++ index.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Portfolio</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+
+    <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a href="#" class="navbar-brand">Portfolio</a>
+        </div>
+        <div class="navbar-collapse collapse">
+          <ul class="nav navbar-nav navbar-right">
+            <li><a class="active" href="#">Home</a></li>
+            <li> <a href="#projects">Projects</a> </li>
+            <li> <a href="#aboutme">About Me</a> </li>
+            <li> <a href="#contact">Contact</a> </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <section class="home" id="home">
+      <div class="container text-center">
+        <h1>Hey This is jin!!</h1>
+        <a href="#">Wanna see my projects?</a>
+      </div>
+    </section>
+
+    <section class="projects" id="projects">
+      <div class="container">
+        <h3>My Work</h3>
+        <div class="row">
+
+          <div class="col-sm-4">
+            <div class="panel panel-default">
+              <div class="panel-heading">
+                Projects
+              </div>
+              <div class="panel-body">
+                <p>Lorem aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+              </div>
+              <div class="panel-footer">
+                <p>ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna </p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-sm-4">
+            <div class="panel panel-default">
+              <div class="panel-heading">
+                Projects
+              </div>
+              <div class="panel-body">
+                <p>Lorem aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+              </div>
+              <div class="panel-footer">
+                <p>ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna </p>
+              </div>
+            </div>
+
+          </div>
+          <div class="col-sm-4">
+            <div class="panel panel-default">
+              <div class="panel-heading">
+                Projects
+              </div>
+              <div class="panel-body">
+                <p>Lorem aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+              </div>
+              <div class="panel-footer">
+                <p>ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna </p>
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="aboutme" id="aboutme">
+      <div class="container">
+        <h1 class="text-center">About Me</h1>
+        <img src="images/system.jpg" class="img-responsive" alt="">
+        <div class="text-center">
+          <p>Hey everyone! this is Jin a Web Developer. i'm from Nepal. i like to make Responsive Website and of course Creative design as well. i work alone since i don't have any friends to work with, and guess what? i enjoy every second of it doing some crazy projecs sometimes i fail but sometimes i do what i wanted to do. Well, that being said i love what i do. and i'm a self taught Web devoloper. </p>
+
+        </div>
+      </div>
+    </section>
+
+    <section class="background">
+
+    </section>
+
+    <footer class="contact" id="contact">
+      <div class="container">
+        <h4>Follow Me</h4>
+        <ul>
+          <li> <a href="http://www.facebook.com">Facebook</a> </li>
+          <li> <a href="http://www.codepen.io/jinrai5777">Codepen</a> </li>
+          <li> <a href="http://www.github.com/jinrai5777">GitHub</a> </li>
+          <li> <a href="http://www.google.com">Gmail</a> </li>
+          <li> <a href="http://www.youtube.com">Youtube</a> </li>
+        </ul>
+      </div>
+    </footer>
+
+
+
+
+    <script type="text/javascript" src="js/jquery.js"></script>
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+  </body>
+</html>
